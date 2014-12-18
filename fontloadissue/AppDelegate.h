9e0b554e545aa0a368a60189243adb5eea898aca
Martin Walsh <martin.walsh@gmail.com>
2014-12-18 15:26:11 +0000

    Initial Commit

---
+++ fontloadissue/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  fontloadissue
+//
+//  Created by Martin Walsh on 18/12/2014.
+//  Copyright Apportable 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ fontloadissue/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  fontloadissue
+//
+//  Created by Martin Walsh on 18/12/2014.
+//  Copyright Apportable 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
